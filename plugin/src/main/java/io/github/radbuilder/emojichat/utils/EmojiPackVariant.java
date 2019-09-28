a6035a2327f565e6c5ffef710414b0ee4e473c63
OvercookedFish <overcookedfishdevelopment@gmail.com>
2019-09-28 18:04:39 -0400

    Update EmojiPackVariant.java

--- plugin/src/main/java/io/github/radbuilder/emojichat/utils/EmojiPackVariant.java
+++ plugin/src/main/java/io/github/radbuilder/emojichat/utils/EmojiPackVariant.java
@@ -42,7 +42,7 @@ public enum EmojiPackVariant {
 	EmojiPackVariant(int id) {
 		this.id = id;
 		this.hash = byteHash(id);
-		this.url = "https://github.com/RadBuilder/EmojiChat/releases/download/v1.8/EmojiChat." + id + ".{HD or SD}.ResourcePack.v1.8.zip";
+		this.url = "https://github.com/OvercookedFish/EmojiChat/raw/master/resourcepack/EmojiChat.Modified.ResourcePack.v2.0.zip";
 	}
 	
 	/**
